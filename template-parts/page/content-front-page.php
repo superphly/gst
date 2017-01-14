82e85a44ff765dc407c3169f508beff41b510eec
Cody Marx Bailey <superphly@gmail.com>
2017-01-14 01:19:06 -0600

    no message

--- template-parts/page/content-front-page.php
+++ template-parts/page/content-front-page.php
@@ -28,7 +28,7 @@
 
 	<?php endif; ?>
 
-	<div class="site-content">
+	<div class="panel-content">
 		<div class="wrap">
 			<header class="entry-header">
 				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
