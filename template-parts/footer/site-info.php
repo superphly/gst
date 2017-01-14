5f5608dc9ef69ecf5c3276f1760f57a00e902756
Cody Marx Bailey <superphly@gmail.com>
2017-01-13 18:56:17 -0600

    no message

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+	<a href="/">Grand Stafford Theater &copy; 2017</a>
 </div><!-- .site-info -->
