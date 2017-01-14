520948376ad7bd26cb6d883ef990c5106c85c914
Cody Marx Bailey <superphly@gmail.com>
2017-01-14 01:09:52 -0600

    no message

--- front-page.php
+++ front-page.php
@@ -20,7 +20,7 @@ get_header(); ?>
 		<?php // Show the selected frontpage content.
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
+				get_template_part( 'template-parts/page/content' );
 			endwhile;
 		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
 			get_template_part( 'template-parts/post/content', 'none' );
